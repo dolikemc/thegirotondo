c09fb321014d8ef794f9eb9711e6c858b1c5011a
Christoph Buchetmann <chr@buchetmann.de>
2018-12-13 20:36:00 +0100

    schedule app

--- registration/admin.py
+++ registration/admin.py
@@ -5,8 +5,9 @@ from registration.models import Registration
 
 class RegistrationAdmin(admin.ModelAdmin):
     model = Registration
-    list_display = ('id', 'child_pre_name', 'child_name', 'email', 'birth_date', 'published', 'start')
-    # list_filter = ('email',)
+    list_display = (
+        'id', 'child_pre_name', 'child_name', 'email', 'birth_date', 'published', 'start', 'mother_profession')
+    list_filter = ('language', 'sex')
 
 
 admin.site.register(Registration, RegistrationAdmin)
